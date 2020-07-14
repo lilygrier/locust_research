e5a2d935116b1da6458ad78eb5c479519ba43e18
Lily Grier <lilygrier@cs.uchicago.edu>
2020-07-14 10:51:05 -0500

    Replace make_df.py

--- make_df.py
+++ make_df.py
@@ -4,12 +4,27 @@ Takes in a string of text and makes a pandas dataframe
 from get_text import clean_page
 import pandas as pd
 import re
+import os
 
 def make_csv():
     '''
     Goes through all files, adds data to a pandas dataframe, exports as csv
     '''
+    root_dir = '../../dataRAW/FAO_Reports'
+    ignore_list = ['JAN_1996', 'FEB_1996', 'MAR_1996', 'APR_1996', 'MAY_1996', 'JUNE_1996']
     df = new_df()
+    for subdir, dirs, files in os.walk(root_dir):
+        dirs[:] = [d for d in dirs if int(d) >= 1996]
+        files[:] = [f for f in files if f not in ignore_list]
+        for f in files:
+            file_path = os.path.join(subdir, f)
+            df = parse_text(file_path, df)
+    csv = df.to_csv(path='../../dataCSV/FAO_Reports/report_text.csv')
+    print("csv added")
+    return df
+
+
+
 
 
 def new_df():
@@ -28,20 +43,25 @@ def parse_text(file_path, df):
     text = clean_page(file_path)
     # find all situation and forecast
     cols = ["YEAR", 'MONTH', 'REGION', 'COUNTRY', 'SITUATION', 'FORECAST']
-    regions = ["WEST AFRICA", 'NORTH-WEST AFRICA', 'EASTERN AFRICA', 'NEAR EAST', 'SOUTH-WEST ASIA']
+    regions = ["WESTERN REGION", "WEST AFRICA", 'NORTH-WEST AFRICA', 'EASTERN AFRICA', 
+    'NEAR EAST', 'SOUTH-WEST ASIA', "CENTRAL REGION", "EASTERN REGION"]
     df = pd.DataFrame(columns=cols)
     rel_text = get_relevant_text(text)
+    print("rel_text is: ", rel_text)
     countries = get_countries(rel_text)
+    print("countries is: ", countries)
     year = int(file_path[-4:])
     month = re.findall(r'.+/(.+)_\d+', file_path)[0]
-    print("month is: ", month[0])
+    region = None
+    #print("month is: ", month)
     
     for country, situation, forecast in countries:
         #print(country)
         country_list = re.split(r",? AND|, ?", country.upper())
         for cty in country_list:
             cty = cty.lstrip()
-            if any(region in cty for region in regions): # if the string contains a region
+            #print("cty is: ", cty)
+            if any(region in cty.upper() for region in regions): # if the string contains a region
                 region_list = cty.split('\n')
                 region = region_list[0]
                 cty = region_list[1]
@@ -49,6 +69,14 @@ def parse_text(file_path, df):
             #if re.match(r'.+\n.+', cty):
 
             cty = re.sub('\n', " ", cty)
+            cty = re.sub(r'AND |and ', "", cty)
+            if not region: # for debugging
+                print("no region!")
+                print('cty is: ', cty)
+                print('file is: ', file_path)
+            if not forecast:
+                print("no forecast!!")
+                print('file is: ', file_path)
             df = df.append({'YEAR': year, 'MONTH': month, 'REGION': region, 'COUNTRY': cty, 
                         'SITUATION': situation, 'FORECAST': forecast}, ignore_index=True)
             
@@ -82,13 +110,15 @@ def get_countries(text):
     # also need to deal with lists of countries that have forecasts but not situations
     countries = re.findall(r'(.+?)(?:\n• SITUATION ?\n(.+?))?\n• FORECAST\n(.+?)(?=$|[^.]+(?:\n• SITUATION ?\n.+)?• FORECAST)', 
                             text, re.DOTALL|re.IGNORECASE)
+    #print("countries: ", countries)
     return countries
 
 def get_relevant_text(text):
     result = re.findall(r'(?:Situation and Forecast)(.+?)(?:Announcements?|Other Locusts\n|Glossary of Terms|Other Species)', 
                         text, flags = re.DOTALL|re.IGNORECASE)[0]
-    print(result)
-    print("type of result: ", type(result))
+    #print(result)
+    #
+    #print("type of result: ", type(result))
     # get rid of headers and footers
     to_keep = []
     for line in result.split('\n'):
